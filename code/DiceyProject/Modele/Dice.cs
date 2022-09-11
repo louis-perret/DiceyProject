b55f47cd8481e40ee77590c3f6e7afab317cfcb1
LouisPerret <louisperret0311@gmail.com>
2022-09-11 15:18:03 +0200

    Création du projet + dépôt de la documentation

---
+++ code/DiceyProject/Modele/Dice.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Modele
+{
+    public class Dice
+    {
+    }
+}
---
+++ code/DiceyProject/Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}

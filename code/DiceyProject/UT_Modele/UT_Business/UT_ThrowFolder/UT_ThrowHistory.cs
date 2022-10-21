281f3b59ba09bbccfc60e928dba093f6f2a6d8c1
Neitah MALVEZIN <bamalvezin>
2022-10-21 15:01:51 +0200

    Début des tests sur ThrowHistory

---
+++ code/DiceyProject/UT_Modele/UT_Business/UT_ThrowFolder/UT_ThrowHistory.cs
@@ -0,0 +1,52 @@
+﻿using Modele.Business.ThrowFolder;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UT_Modele.UT_Business.UT_ThrowFolder
+{
+    public class UT_ThrowHistory
+    {
+        [Fact]
+        public void Test_Constuctor_No_Data()
+        {
+            ThrowHistory th = new ThrowHistory();
+            Assert.Equal(th.History, new ReadOnlyDictionary<DateOnly, ListThrowEncapsulation>(new Dictionary<DateOnly, ListThrowEncapsulation>()));
+        }
+
+        private static IEnumerable<object[]> Data_Test_Constructor_Data()
+        {
+            yield return new object[]
+            {
+                0,
+                null,
+                true
+            };
+
+            yield return new object[]
+            {
+                0,
+                new Dictionary<DateOnly,ListThrowEncapsulation>(),
+                false
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(Data_Test_Constructor_Data))]
+        public void Test_Constructor_Data(int expectedCount, Dictionary<DateOnly,ListThrowEncapsulation> dico, bool exception)
+        {
+            if(exception)
+                Assert.Throws<ArgumentNullException>(() => new ThrowHistory(dico));
+            else
+            {
+                ThrowHistory history = new ThrowHistory(dico);
+                Assert.Equal(dico, history.History);
+            }
+
+        }
+
+    }
+}
